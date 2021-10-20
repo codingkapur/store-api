689d2abcf117a1e2265979316c2e957cc73734f1
Raghav <codingkapur@gmail.com>
2021-10-20 22:47:41 +0530

    implement process.exit after dynamically populating database

--- populate.js
+++ populate.js
@@ -13,8 +13,11 @@ const start = async () => {
         await Product.deleteMany();
         await Product.create(jsonProducts)
         console.log('Success');
+        //Set up a process.exit to automatically exit once the database has been populated and this function has served its purpose.
+        process.exit(0);
     } catch (error) {
         console.log(error);
+        process.exit(1);
     }
 }
 
